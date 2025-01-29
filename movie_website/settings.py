64a6ed880e257739a127d9a22597f72c65e6bb10
DavidPanGithub <davidusinggithub@gmail.com>
2025-01-30 06:25:17 +0800

    minor change

--- movie_website/settings.py
+++ movie_website/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'landing/static'),  # Path to your app's static files
+    os.path.join(BASE_DIR, 'landing/static'),  # Path to app's static files
 ]
 
 # Default primary key field type

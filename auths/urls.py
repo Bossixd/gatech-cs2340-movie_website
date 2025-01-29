5cdc4aaadc5d4d79d93796ea3c9b2226977e3a06
Bossixd <bosspattakit123@gmail.com>
2025-01-29 17:09:38 -0500

    finished login and registration page

--- auths/urls.py
+++ auths/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = "auths"
 urlpatterns = [
-    path("register/", views.registerPage, name="register"),
-    # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote")
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
--- auths/views.py
+++ auths/views.py
@@ -1,5 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.models import auth
+from django.core.validators import validate_email
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.hashers import make_password
+
+from django.contrib.auth.models import User
 
 # Create your views here.
-def registerPage(request):
-    return render(request, 'auths/register.html')
+def register(request):
+    if request.method == "GET":
+        return render(request, 'auths/register.html', {})
+    elif request.method == "POST":
+        if User.objects.filter(username=request.POST["username"]).exists():
+            return render(request, 'auths/register.html', {
+                "error_type": "username",
+                "error": "Username already exists!"
+            })
+
+        try:
+            validate_email(request.POST["email"])
+        except:
+            return render(request, 'auths/register.html', {
+                "error_type": "email",
+                "error": "Email is not valid!"
+            })
+
+        if User.objects.filter(email=request.POST["email"]).exists():
+            return render(request, 'auths/register.html', {
+                "error_type": "email",
+                "error": "Email already exists!"
+            })
+            
+        try:
+            validate_password(request.POST["password"])
+        except:
+            return render(request, 'auths/register.html', {
+                "error_type": "password",
+                "error": "Password is not strong enough!"
+            })
+
+        user = User(username=request.POST["username"], email=request.POST["email"], password=make_password(request.POST["password"]))
+        user.save()
+        return HttpResponseRedirect(reverse("auths:login"))
+
+def login(request):
+    if (request.method == "GET"):
+        return render(request, 'auths/login.html')
+    elif request.method == "POST":
+        user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("landing:landing_page"))
+    
+    return render(request, 'auths/login.html', {
+            "error": "Invalid Login Credentials"
+        })
+
+def logout(request):
+    auth.logout(request)
+    return redirect("")
+
+# @login_required(login_url="auths:login")
+
+
+
+
+
+

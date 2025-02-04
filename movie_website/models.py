93f23b5544a3d85e85e46b2a4385287db65cb735
jessebesties <137125199+jessebesties@users.noreply.github.com>
2025-02-03 22:27:31 -0500

    added a search function and a new review field

--- movie_website/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from django.contrib.postgres.fields import ArrayField
-
-class Movie(models.Model):
-    movieName = models.CharField(maxlength=200)
-
-    def __str__(self):
-        return self.movieName
--- movies/admin.py
+++ movies/admin.py
@@ -3,6 +3,6 @@ from .models import Movie
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_date', 'duration')
+    list_display = ('title', 'release_date', 'duration', 'reviews')
     search_fields = ('title', 'description')
     list_filter = ('release_date',)

092e4c02e8404df02c4c1d461bc97134b78169f6
DavidPanGithub <davidusinggithub@gmail.com>
2025-02-08 00:32:14 +0800

    added connection between landingpage and movie_list

--- movie_list/urls.py
+++ movie_list/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import movie_detail, movie_list
 
-import movies
-from . import views
-
+app_name = "movie_list"
 urlpatterns = [
     path('', movie_list, name='movie_list'),
     path('movies/<int:pk>/', movie_detail, name='movie_detail'),

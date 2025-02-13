4584eba43fe9a2ace1cc2e81caeeec37bccf7f0e
Bossixd <bosspattakit123@gmail.com>
2025-02-13 01:14:22 -0500

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,12 @@
 echo "Upgrading pip..."
 python3 -m pip install --upgrade pip
 
+echo "installing sqlite3"
+sudo apt install libsqlite3-dev
+pyenv install 3.11.3
+
 echo "Building project packages..."
 python3 -m pip install -r requirements.txt
-python3 -m pip install install sqlite-devel -y
 
 echo "Migrating Database..."
 python3 manage.py makemigrate --noinput

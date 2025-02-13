cab7cdc90cbe3fe04467198d3ba40022886b4aaf
Bossixd <bosspattakit123@gmail.com>
2025-02-13 01:09:39 -0500

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,9 @@
+echo "Upgrading pip..."
+python3 -m pip install --upgrade pip
+
 echo "Building project packages..."
 python3 -m pip install -r requirements.txt
+python3 -m pip install install sqlite-devel -y
 
 echo "Migrating Database..."
 python3 manage.py makemigrate --noinput
